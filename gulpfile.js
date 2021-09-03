b4c40858b6b2baa8da91bbd70b59fc3c703caaf7
N3P0m <dicobraz47@gmail.com>
2021-09-03 07:51:56 +0300

    Optimizing images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
+const rename = require("gulp-rename");
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const gulpStylelint = require('gulp-stylelint');
+const cleanCSS = require('gulp-clean-css');
 
 
 function style () {
@@ -18,19 +20,28 @@ function style () {
     .pipe(gulp.dest('./assets/css'))
     .pipe(browserSync.stream())
 }
+function style () {
+    return gulp.src('./assets/scss/**/*.scss')
+    .pipe(gulpStylelint({
+        reporters: [
+          {
+              formatter: 'string',
+               console: true
+          }
+        ]
+    }))
+    .pipe(sass())
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream())
+}
 
-    
-function lint () {
- 
-  return gulp.src('./assets/scss/**/*.scss')
-        .pipe(gulpStylelint({
-         reporters: [
-        {
-            formatter: 'string',
-             console: true
-            }
-      ]
-    }));
+function min() {
+      return gulp.src('./assets/css/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+            path.extname = ".css";}))
+        .pipe(gulp.dest('./assets/css'));
 }
 
 function watch () {
@@ -47,7 +58,7 @@ function watch () {
 exports.style = style;
 // exports.lint = lint;
 exports.watch = watch;
+exports.min = min;
 
 
 
- 
